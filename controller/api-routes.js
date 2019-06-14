06fbfc1cb4d4651e924c6fdf1ddc264195f8725d
Mike Slaby <mikeslaby3@gmail.com>
2019-06-14 16:59:57 -0700

    Post request for forget password

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -1,6 +1,7 @@
 var db = require("../models");
 var passport = require("../config/passport");
-
+var nodemailer = require('nodemailer');
+var async = require('async');
 
 module.exports = function (app) {
     app.get("/members/:username", function (req, res) {
@@ -35,6 +36,7 @@ module.exports = function (app) {
             });
         })
     })
+
     app.post("/api/signup", function (req, res) {
         console.log(req.body);
         db.User.create({
@@ -80,6 +82,60 @@ module.exports = function (app) {
 
     app.post("/api/login", passport.authenticate("local"), function (req, res) {
         res.json("/members");
+    });
+
+    app.post('/forgot', function (req, res, next) {
+        // Here we are using async module to avoid nesting callbacks within callbacks within callbacks.
+        async.waterfall([
+            // Generates a unique token for password reset
+            function (done) {
+                crypto.randomBytes(20, function (err, buf) {
+                    var token = buf.toString('hex');
+                    done(err, token);
+                });
+            },
+            function (token, done) {
+                db.User.findOne({ email: req.body.email }, function (err, user) {
+                    if (!user) {
+                        req.flash('error', 'No account with that email address exists.');
+                        return res.redirect('/forgot');
+                    }
+                    // If email exists in db, we assign the token to the user
+                    user.resetPasswordToken = token;
+                    // Password reset link expires in an hour
+                    user.resetPasswordExpires = Date.now() + 3600000;
+
+                    user.save(function (err) {
+                        done(err, token, user);
+                    });
+                });
+            },
+            function (token, user, done) {
+                const smtpTransport = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'mikenodemailer@gmail.com',
+                        pass: 'n0dep@ssword18'
+                    }
+                });
+                let mailOptions = {
+                    to: user.email,
+                    from: 'mikenodemailer@gmail.com',
+                    subject: 'Congregeek Password Reset',
+                    text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                        'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                        'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                        'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+                };
+                smtpTransport.sendMail(mailOptions, function (err) {
+                    req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+                    done(err, 'done');
+                });
+            }
+        ], function (err) {
+            if (err) return next(err);
+            res.redirect('/forgot');
+        });
     });
 
 }
