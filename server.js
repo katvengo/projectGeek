7e2f631c7cc8a65c299c077b64daa4e602bc53c2
Kat Vengo <katvengo@gmail.com>
2019-06-13 21:51:01 -0700

    updated folder structure

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var app = express();
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
